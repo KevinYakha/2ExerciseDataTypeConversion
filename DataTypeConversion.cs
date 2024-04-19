4ef6ed29f82d9c13756e01b3902cbcbe121e5494
KevinYakha <kevin@yakha.eu>
2024-04-19 11:40:19 +0200

    added initial code for the second exercise

--- DataTypeConversion.cs
+++ DataTypeConversion.cs
@@ -5,6 +5,11 @@ namespace DataTypeConversion
     class DataTypeConversion()
     {
         public static string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+        int value1 = 11;
+        decimal value2 = 6.2m;
+        float value3 = 4.3f;
+
         public static void Main(string[] args)
         {
             CultureInfo.CurrentCulture = new CultureInfo("en-US");
@@ -20,6 +25,16 @@ namespace DataTypeConversion
             }
             Console.WriteLine("Message: " + message);
             Console.WriteLine("Total: " + sum);
+
+            // Your code here to set result1
+            // Hint: You need to round the result to nearest integer (don't just truncate)
+            Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+            // Your code here to set result2
+            Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+            // Your code here to set result3
+            Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
         }
     }
 }
