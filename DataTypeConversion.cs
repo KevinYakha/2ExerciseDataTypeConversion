75f0bd878865d2a605f9f3de19a077d18f21d1b5
KevinYakha <kevin@yakha.eu>
2024-04-19 11:38:56 +0200

    solved the first challenge using TryParse

--- DataTypeConversion.cs
+++ DataTypeConversion.cs
@@ -1,11 +1,25 @@
-﻿namespace DataTypeConversion
+﻿using System.Globalization;
+
+namespace DataTypeConversion
 {
     class DataTypeConversion()
     {
-        string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+        public static string[] values = { "12.3", "45", "ABC", "11", "DEF" };
         public static void Main(string[] args)
         {
+            CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
+            float sum = 0.0f;
+            string message = "";
+            for(int i = 0; i < values.Length; i++)
+            {
+                if (float.TryParse(values[i], out float result))
+                    sum += result;
+                else
+                    message += values[i];
+            }
+            Console.WriteLine("Message: " + message);
+            Console.WriteLine("Total: " + sum);
         }
     }
 }
